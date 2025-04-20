acc37648b2435f35abb342b0f41bd3d346b08ffd
Aashish Kumar <166286708+Aashish1A@users.noreply.github.com>
2025-04-20 18:14:22 +0530

    Learnt about rtk query and completed fundamental learning of redux

---
+++ RTK_QUERY/src/apiSlice.js
@@ -0,0 +1,14 @@
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react"
+
+
+export const api = createApi({
+    baseQuery: fetchBaseQuery({baseUrl : "http://localhost:5173"}),
+    endpoints: (builder) => {
+        getTasks: builder.query({
+            query: () => "/tasks",
+        })
+    }
+})
+
+
+export const  { useGetTaskQuery } = api;
